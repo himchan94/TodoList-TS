42e85d3855b467456fdf7b80ab21ad1278fc1fe5
himchan94 <himchan9494@gmail.com>
2022-05-03 04:38:47 +0900

    feat: Install react-router, add-routes

---
+++ src/screens/ListScreen.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+type Props = {};
+
+const ListScreen: React.FunctionComponent<Props> = (props) => {
+  return <div>List view</div>;
+};
+
+export default ListScreen;
