b6e03b9125d3a4246b4ea5b1c6cba60a3613b996
himchan94 <himchan9494@gmail.com>
2022-06-05 23:39:46 +0900

    feat: Apply NavLink

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+import FocusScreen from './screens/FocusScreen';
 import ListScreen from './screens/ListScreen';
+
+const activeStyle = {
+  fontWeight: 'bold',
+};
 
 function App() {
   return (
     <BrowserRouter>
+      <nav>
+        <NavLink
+          to="/"
+          style={({ isActive }) =>
+            isActive ? activeStyle : { fontWeight: 'normal' }
+          }
+        >
+          List
+        </NavLink>{' '}
+        -{' '}
+        <NavLink
+          to="focus"
+          style={({ isActive }) =>
+            isActive ? activeStyle : { fontWeight: 'normal' }
+          }
+        >
+          Focus
+        </NavLink>
+      </nav>
       <Routes>
         <Route path="/" element={<ListScreen />} />
-        <Route path="/focus" />
+        <Route path="/focus" element={<FocusScreen />} />
       </Routes>
     </BrowserRouter>
   );
